ae6502aa12f2cd8d45245ad0caf2f133f779e9f3
booayl <angelaboo226@icloud.com>
2024-05-08 16:27:24 +0100

    user can now delete their own comment

--- The-Puffin-Room/src/components/CommentCard.jsx
+++ The-Puffin-Room/src/components/CommentCard.jsx
@@ -1,27 +1,42 @@
 import { LoginContext } from "../contexts/LoginContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+
+import { deleteComment } from "../api";
 
 import dateFormat from "dateformat";
-import moment from 'moment';
+import moment from "moment";
 moment().format();
 
-function CommentCard({ comment }) {
-const { loggedUser } = useContext(LoginContext);
+function CommentCard({ comment , onDelete}) {
+  const date = dateFormat(comment.created_at, "isoDate");
+  const howLongAgo = moment(date, "YYYYMMDD").fromNow();
 
-const date = dateFormat(comment.created_at, "isoDate");
-const howLongAgo= moment(date, "YYYYMMDD").fromNow();
+  const { loggedUser } = useContext(LoginContext);
 
-if(!comment.author){
-  comment.author = loggedUser.username
-}
+  if (!comment.author) {
+    comment.author = loggedUser.username;
+  }
+
+  const handleClick = () => {
+    console.log(comment.comment_id)
+    console.log("clicked")
+    onDelete(comment.comment_id)
+    deleteComment(comment.comment_id)
+  };
 
   return (
-  <div className="commentCard">
-<>{comment.author} . {howLongAgo}</>
-{comment.author === loggedUser.username ? <button>x</button>: null}
-<p>{comment.body}</p>
-<img src="https://i.ibb.co/7Wvj2Pm/votes.png"/>{comment.votes}</div>
-);
+    <div className="commentCard">
+      <>
+        {comment.author} . {howLongAgo}
+      </>
+      {comment.author === loggedUser.username ? (
+        <div onClick={handleClick}><button>x</button></div>
+      ) : null}
+      <p>{comment.body}</p>
+      <img src="https://i.ibb.co/7Wvj2Pm/votes.png" />
+      {comment.votes}
+    </div>
+  );
 }
 
 export default CommentCard;
